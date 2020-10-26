d5b1fe8afee10ac2d3356926bc1cf134fe46f34b
s.giuri <s.giuri@issm.it>
2020-10-26 13:47:02 +0100

    Primi Passi

---
+++ allergies.py
@@ -0,0 +1,27 @@
+from math import log2
+
+class Allergies:
+
+    def __init__(self, score):
+        self.score = score
+        for j in [2**n for n in range(int(log2(self.score)))+1]:
+            if
+        pass
+
+    def allergic_to(self, item):
+        pass
+
+    @property
+    def lst(self):
+        allergenes={
+        "eggs": (1),
+        "peanuts": (2),
+        "shellfish": (4),
+        "strawberries": (8),
+        "tomatoes": (16),
+        "chocolate": (32),
+        "pollen": (64),
+        "cats": (128)}
+
+
+        pass
